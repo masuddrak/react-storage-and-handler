fb6c0756b0fcfdfc431cefe8a060dd2bb34a4e41
masud rana <masudranaacb@gmail.com>
2022-03-21 19:16:27 +0600

    using function parametar even heandalar

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Display = (props) => {
     const {name,id,price}=props.cosmatic;
+    const addToCard=(id)=>{
+        console.log(id)
+    }
     return (
         <div>
             <h3>Name: {name}</h3>
             <p>id: {id}</p>
             <p>Price: {price}</p>
+            <button onClick={()=>addToCard(id)}>Ad to Card</button>
         </div>
     );
 };
