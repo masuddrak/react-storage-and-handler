032039d3f48789d203514e04b4f1fd87a71fd5b9
masud rana <masudranaacb@gmail.com>
2022-03-21 10:32:51 +0600

    object export

--- src/components/Cosmatics/Cosmatics.js
+++ src/components/Cosmatics/Cosmatics.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { add, multiplay } from '../../ustilitis/calculate';
+
 
 const Cosmatics = () => {
+    const total=add(10,12)
+    const multipay=multiplay(10,12)
     return (
         <div>
             <h2>Welcome To My Cosmetics </h2>
+            <p>total:{total}</p>
+            <p>multipay:{multipay}</p>
         </div>
     );
 };
---
+++ src/ustilitis/calculate.js
@@ -0,0 +1,8 @@
+const add=(first,second)=>{
+    return first+second;
+}
+const multiplay=(first,second)=>{
+    return first*second;
+}
+
+export  {add,multiplay};
